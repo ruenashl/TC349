b020b4a5b197814ab85d26828d28141a887a37db
Ashley Ruen <ruenashl@msu.edu>
2013-05-02 10:29:49 -0400

    final

--- projects/final/js/script.js
+++ projects/final/js/script.js
@@ -5,3 +5,25 @@ $('.button').click(function(){
 myQuote.show(500);
 });
 });
+
+<script type="text/javascript">
+$(function() {
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('#toTop').fadeIn();	
+		} else {
+			$('#toTop').fadeOut();
+		}
+	});
+ 
+	$('#toTop').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});	
+});
+</script>
+
+
+<script>
+  $(document).ready(function(){
+    $('#sidebar').stickyMojo({footerID: '#footer', contentID: '#main'});
+  });
